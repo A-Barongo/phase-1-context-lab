b72c6fae0653f0d515063245a9d20fac2205b3f7
Allan <allanbarongo5@gmail.com>
2025-03-28 12:06:22 +0300

    completed phase 1 context lab

--- index.js
+++ index.js
@@ -1,5 +1,52 @@
 /* Your Code Here */
 
+function createEmployeeRecord([firstName, familyName, title, payPerHour]) {
+    return {
+        firstName,
+        familyName,
+        title,
+        payPerHour,
+        timeInEvents: [],
+        timeOutEvents: []
+    };
+}
+
+function createEmployeeRecords(employeeData) {
+    return employeeData.map(createEmployeeRecord);
+}
+
+function createTimeInEvent(dateTimeString) {
+    let [date, hour] = dateTimeString.split(" ");
+    this.timeInEvents.push({ type: "TimeIn", date, hour: parseInt(hour, 10) });
+    return this;
+}
+
+function createTimeOutEvent(dateTimeString) {
+    let [date, hour] = dateTimeString.split(" ");
+    this.timeOutEvents.push({ type: "TimeOut", date, hour: parseInt(hour, 10) });
+    return this;
+}
+
+function hoursWorkedOnDate(date) {
+    let inEvent = this.timeInEvents.find(e => e.date === date);
+    let outEvent = this.timeOutEvents.find(e => e.date === date);
+    return (outEvent.hour - inEvent.hour) / 100;
+}
+
+function wagesEarnedOnDate(date) {
+    return hoursWorkedOnDate.call(this, date) * this.payPerHour;
+}
+
+
+function findEmployeeByFirstName(collection, firstNameString) {
+    return collection.find(emp => emp.firstName === firstNameString);
+}
+
+function calculatePayroll(employeeRecords) {
+    return employeeRecords.reduce((total, emp) => total + allWagesFor.call(emp), 0);
+}
+
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
